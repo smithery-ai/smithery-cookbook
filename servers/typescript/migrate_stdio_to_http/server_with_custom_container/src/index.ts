46b0390b7a75646d117e521e51e6bbfb1389e228
arjunkmrm <arjunkmrm@gmail.com>
2025-08-14 22:39:23 +0800

    update tool res

--- servers/typescript/migrate_stdio_to_http/server_with_custom_container/src/index.ts
+++ servers/typescript/migrate_stdio_to_http/server_with_custom_container/src/index.ts
@@ -64,7 +64,7 @@ export default function createServer({
     return { 
       content: [{ 
         type: "text", 
-        text: `Hello, ${name}! (authenticated with ${config.apiKey.substring(0, 8)}...)` 
+        text: `Hello, ${name}!` 
       }] 
     };
   });
